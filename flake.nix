386f1b0ceb593a2634ec26670c531cc56c122b03
toyama170402 <toyamasoujinn@gmail.com>
2026-06-25 10:53:24 +0900

    実行時エラーの低減
    不要なフラグの削除
    実行時に必要なプロジェクトの追加
    実行時に必要なライブラリの追加
      それに伴うサポートするアーキテクチャの縮小

--- flake.nix
+++ flake.nix
@@ -8,33 +8,39 @@
 
   outputs = {
     nixpkgs,
-    flake-utils,
     ...
   }:
-  flake-utils.lib.eachDefaultSystem (
-    system: 
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.default = pkgs.buildDotnetModule {
-        pname = "Beutl";
-        version = "2.0.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "b-editor";
-          repo = "beutl";
-          rev = "a24a93c211001f05eeb626691b9ae89cb1780390";
-          hash = "sha256-peWRmgjhQD+DGz7k8Wiydzvu73I1EIRAGSZKw296f7Y=";
-        };
-        projectFile = "src/Beutl/Beutl.csproj";
-        dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0_3xx;
-        dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0-bin;
-        nugetDeps = ./deps.json;
-        selfContainedBuild = true;
-        dotnetBuildFlags = [ "-p:TargetFramework=net10.0" ];
-        dotnetRestoreFlags = [ "-p:TragetFramework=net10.0" ];
-        dotnetInstallFlags = [ "-p:TargetFramework=net10.0" ];
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.buildDotnetModule {
+      pname = "Beutl";
+      version = "2.0.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "b-editor";
+        repo = "beutl";
+        rev = "a24a93c211001f05eeb626691b9ae89cb1780390";
+        hash = "sha256-peWRmgjhQD+DGz7k8Wiydzvu73I1EIRAGSZKw296f7Y=";
       };
-    }
-  );
+      projectFile = [
+          "src/Beutl/Beutl.csproj"
+          "src/Beutl.FFmpegWorker/Beutl.FFmpegWorker.csproj"
+        ];
+      dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0_3xx;
+      dotnet-runtime = pkgs.dotnetCorePackages.runtime_10_0-bin;
+      nugetDeps = ./deps.json;
+      selfContainedBuild = true;
+      dotnetBuildFlags = [ "-p:TargetFramework=net10.0" ];
+      dotnetInstallFlags = [ "-p:TargetFramework=net10.0" ];
+
+      runtimeDeps = with pkgs; [
+        libx11
+        libxcursor
+        libxext
+        ffmpeg
+      ];
+    };
+  };
 }
